e5e64498b7d6f5d68bd71adbbf5a0ed5e704f1cd
mirgubad <Mirgubad@MIRGUBAD>
2024-04-16 17:56:26 +0400

    task-2 changed _swap added

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,31 +1,43 @@
 #include "sort.h"
 /**
+ * _swap - swap two numbers
+ * @a: integer
+ * @b: integer
+ **/
+void _swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+/**
  * selection_sort - sort array using selection sort algorithm
  * @array: array
  * @size: array size
  **/
+
 void selection_sort(int *array, size_t size)
 {
-    unsigned int i, j, min;
+	unsigned int i, j, min;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size; i++)
-    {
-        min = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[min] > array[j])
-                min = j;
-        }
-        if (min != i)
-        {
-            // Swap elements without using _swap method
-            int temp = array[i];
-            array[i] = array[min];
-            array[min] = temp;
-            // You can print the array here if needed
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+				min = j;
+		}
+		if (min != i)
+		{
+			_swap(&array[i], &array[min]);
+			print_array(array, size);
+		}
+	}
+
 }
